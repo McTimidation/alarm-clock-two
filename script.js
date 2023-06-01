9414d8207ae19f93ebdedcfc19c7cdc49422ca29
Michael McGraw <michaelmcgraw09@gmail.com>
2023-06-01 14:10:25 -0400

    twerkin

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-
-
+let displayTime
+let alarmTime = []
+let alarmInput
 function getTime() {
     let date = new Date();
-    document.getElementById("displayTime").innerHTML=date;
+    displayTime = document.getElementById("displayTime").innerHTML=date.toLocaleTimeString();
+
+}
+
+function setAlarm() {
+alarmInput = document.getElementById("alarmInput").value
+console.log(alarmInput.toLocaleTimeString())
 }
 
 setInterval(getTime, 1000);
