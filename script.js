6a751c4a7de0ccd19fcf9e2bfc25a1ff322b393f
Michael McGraw <michaelmcgraw09@gmail.com>
2023-06-02 11:56:05 -0400

    fix

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let displayTime
-let alarmTime = []
+let alarmTime
 let alarmInput
+
 function getTime() {
     let date = new Date();
     displayTime = document.getElementById("displayTime").innerHTML=date.toLocaleTimeString();
+}
 
+function convertTime(time) {
+    TA = time.split(":");
+    let h = parseInt(TA[0]) % 12;
+    let s = parseInt(TA[1]); 
+    let amOrPm = (h <= 12) ? 'AM' : 'PM';
+    let hours = (h = 0) ? 12 : h;
+    return `${hours}:${s}:00 ${amOrPm}`
 }
 
 function setAlarm() {
 alarmInput = document.getElementById("alarmInput").value
-console.log(alarmInput.toLocaleTimeString())
+let newAlarm = document.createElement("p");
+newAlarm.innerText = convertTime(alarmInput);
+document.getElementById("displayContainer").appendChild(newAlarm);
+newAlarm.addEventListener('click', () => {
+    newAlarm.remove();
+})
+
 }
 
+
+
+setInterval(console.log(alarmInput), 5000);
 setInterval(getTime, 1000);
